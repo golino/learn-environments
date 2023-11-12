05d9ffee7c6c117ad6c1f76365f159ec16eddb2e
Pedro Golino <golino@gmail.com>
2023-11-12 15:28:10 -0300

    adjust

--- github/runners/build.sh
+++ github/runners/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-buildBase = "docker build -t github-runner-base:$ -f base/Dockerfile ."
+GH_RUNNER_BASE_NAME='github-runner-base'
+GH_RUNNER_BASE_VERSION='0.1'
+
+GH_RUNNER_NAME='github-runner'
+GH_RUNNER_VERSION='0.1'
+
+buildBase = "docker build -t $GH_RUNNER_BASE_NAME:$GH_RUNNER_BASE_VERSION -f base/Dockerfile ."
